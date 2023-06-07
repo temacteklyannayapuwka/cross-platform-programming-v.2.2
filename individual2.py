e78d27413d95781fc45a65d77deaf41a8ac52d54
Artem Bakanov <rtmbakanov@gmail.com>
2023-06-08 02:47:31 +0300

    add individual 2 and 3.py

---
+++ individual2.py
@@ -0,0 +1,14 @@
+import sys
+
+a = float(input("Введите число a: "))
+b = float(input("Введите число b: "))
+c = float(input("Введите число c: "))
+
+if abs(a) >= 4:
+    print("Модуль числа a не меньше 4.")
+
+if abs(b) >= 4:
+    print("Модуль числа b не меньше 4.")
+
+if abs(c) >= 4:
+    print("Модуль числа c не меньше 4.")
